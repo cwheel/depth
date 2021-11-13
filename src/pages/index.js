5f7b44f258a4c0b1f518898552af77a5acdc30b2
Cameron Wheeler <cwheel@users.noreply.github.com>
2021-11-12 19:49:43 -0500

    very basic styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,5 @@
+import './global.css'
+
 import * as React from 'react'
 import { Helmet } from 'react-helmet'
 import styled from '@emotion/styled';
@@ -5,16 +7,29 @@ import styled from '@emotion/styled';
 import { planOC, planCCR } from '../planner';
 
 const PageContainer = styled.div`
-    margin: 0;
-    padding: 0;
-
     display: flex;
     flex-direction: column;
+
+    margin-left: 50px;
+    margin-right: 50px;
+    margin-top: 50px;
 `;
 
 const FormGroup = styled.div`
     display: flex;
     flex-direction: column;
+
+    input {
+        margin-bottom: 10px;
+    }
+
+    select {
+        margin-bottom: 10px;
+    }
+
+    button {
+        margin-top: 30px;
+    }
 `;
 
 const Disclaimer = styled.div`
@@ -24,10 +39,17 @@ const Disclaimer = styled.div`
     left: 0;
     right: 0;
 
-    height: 100%;
-    width: 100%;
+    height: auto;
+    width: auto;
+
+    padding: 50px;
 
     background: white;
+`;
+
+const RejectButton = styled.button`
+    background: #f1f1f1;
+    margin-left: 10px;
 `;
 
 const IndexPage = () => {
@@ -45,7 +67,7 @@ const IndexPage = () => {
     });
 
     const [result, setResult] = React.useState('');
-    const [showDisclaimer, setShowDisclaimer] = React.useState(true);
+    const [showDisclaimer, setShowDisclaimer] = React.useState(false);
 
     React.useEffect(() => {
         const formState = JSON.parse(window.localStorage.getItem('form'));
@@ -81,45 +103,51 @@ const IndexPage = () => {
             </Helmet>
 
             {showDisclaimer && <Disclaimer>
+                <h1>Depth Disclaimer</h1>
                 <p>Calculations made with Depth should not be relied upon and no guarantees of accuracy are made. SCUBA diving, especially that which occurs in an overhead environment is inherently dangerous and requires specific training. Use Depth at your own risk.</p>
                 <button onClick={hideDisclaimer}>Accept</button>
-                <button onClick={() => window.location = 'http://example.com'}>Decline</button>
+                <RejectButton onClick={() => window.location = 'http://example.com'}>Decline</RejectButton>
             </Disclaimer>}
 
             <PageContainer>
-                <label for='mode'>Mode</label>
-                <select name='mode' onChange={updateForm} value={form.mode}>
-                    <option value='ccr'>Closed Circut Rebreather</option>
-                    <option value='oc'>Open Circut</option>
-                </select>
+                <h1>Depth</h1>
 
-                <label for='salinity'>Water Type</label>
-                <select name='salinity' onChange={updateForm} value={form.salinity}>
-                    <option value='fresh'>Fresh Water</option>
-                    <option value='salt'>Salt Water</option>
-                </select>
+                <FormGroup>
+                    <label for='mode'>Mode</label>
+                    <select name='mode' onChange={updateForm} value={form.mode}>
+                        <option value='ccr'>Closed Circut Rebreather</option>
+                        <option value='oc'>Open Circut</option>
+                    </select>
 
-                <label for='totalGas'>Total Gas (cf.)</label>
-                <input placeholder='Total Gas (cf.)' type='number' step='0.01' name='totalGas' onChange={updateForm} value={form.totalGas} />
+                    <label for='salinity'>Water Type</label>
+                    <select name='salinity' onChange={updateForm} value={form.salinity}>
+                        <option value='fresh'>Fresh Water</option>
+                        <option value='salt'>Salt Water</option>
+                    </select>
 
-                <label for='depth'>Depth (ft.)</label>
-                <input placeholder='Depth' type='number' step='0.01' name='depth' onChange={updateForm} value={form.depth} />
+                    <label for='totalGas'>Total Gas (cf.)</label>
+                    <input placeholder='Total Gas (cf.)' type='number' step='0.01' name='totalGas' onChange={updateForm} value={form.totalGas} />
 
-                {form.mode === 'oc' && <FormGroup>
-                    <label for='penetrationRate'>Penetration Rate (ft/min)</label>
-                    <input placeholder='Penetration Rate (ft/min)' type='number' step='0.01' name='penetrationRate' onChange={updateForm} value={form.penetrationRate} />
+                    <label for='depth'>Depth (ft.)</label>
+                    <input placeholder='Depth' type='number' step='0.01' name='depth' onChange={updateForm} value={form.depth} />
 
-                    <label for='sacPenetration'>Penetration SAC Rate</label>
-                    <input placeholder='Penetration SAC Rate' type='number' step='0.01' name='sacPenetration' onChange={updateForm} value={form.sacPenetration}/>
-                </FormGroup>}
+                    {form.mode === 'oc' && <FormGroup>
+                        <label for='penetrationRate'>Penetration Rate (ft/min)</label>
+                        <input placeholder='Penetration Rate (ft/min)' type='number' step='0.01' name='penetrationRate' onChange={updateForm} value={form.penetrationRate} />
 
-                <label for='exitRate'>Exit Rate (ft/min)</label>
-                <input placeholder='Exit Rate (ft/min)' type='number' step='0.01' name='exitRate' onChange={updateForm} value={form.exitRate} />
+                        <label for='sacPenetration'>Penetration SAC Rate</label>
+                        <input placeholder='Penetration SAC Rate' type='number' step='0.01' name='sacPenetration' onChange={updateForm} value={form.sacPenetration}/>
+                    </FormGroup>}
 
-                <label for='sacExit'>Exit SAC Rate</label>
-                <input placeholder='Exit SAC Rate' type='number' step='0.01' name='sacExit' onChange={updateForm} value={form.sacExit} />
+                    <label for='exitRate'>Exit Rate (ft/min)</label>
+                    <input placeholder='Exit Rate (ft/min)' type='number' step='0.01' name='exitRate' onChange={updateForm} value={form.exitRate} />
 
-                <button onClick={calculate}>Calculate</button>
+                    <label for='sacExit'>Exit SAC Rate</label>
+                    <input placeholder='Exit SAC Rate' type='number' step='0.01' name='sacExit' onChange={updateForm} value={form.sacExit} />
+
+                    <button onClick={calculate}>Calculate</button>
+                </FormGroup>
+
                 {result && <b>Maximum of {result} ft.</b>}
             </PageContainer>
         </>
